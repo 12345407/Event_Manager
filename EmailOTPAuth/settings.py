8008dda2675c540beea5a86d75f9c2cd045b2114
Rahul Jha <jharahul1195@gmail.com>
2023-03-28 22:58:10 +0530

    minor changes

--- EmailOTPAuth/settings.py
+++ EmailOTPAuth/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '!@*2nva@$tsw)2eso!j(596r!w)!5f_#lf7dj!m-r9ikn169@4'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['otpauth.herokuapp.com', '127.0.0.1']
+ALLOWED_HOSTS = ['127.0.0.1']
 
 # Application definition
 
